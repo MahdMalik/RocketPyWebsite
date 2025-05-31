0c979f31afe8e9dd6995e56b866d0d0bc37926e7
MahdMalik <mahdmahdmalik@gmail.com>
2025-05-31 18:17:52 -0500

    basic pages set up, now have to actually set up stuff

---
+++ app/landing/page.js
@@ -0,0 +1,10 @@
+'use client'
+
+export default function LandingPage()
+{
+    return (
+        <div>
+            <p>I LOVE KANYE</p>
+        </div>
+    )
+}
---
+++ app/register/page.js
@@ -0,0 +1,10 @@
+'use client'
+
+export default function Register()
+{
+    return (
+        <div>
+            <p>I LOVE THE KANYE ALBUM MY BEAUTIFUL DARK TWISTED FANTASY GRAHH</p>
+        </div>
+    )
+}
